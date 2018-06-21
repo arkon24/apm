3bf2171137abc1f43841fc2f644a5b6cd4d3beed
Aaron Stennett <srf.aquila001@gmail.com>
2018-06-21 14:20:07 -0500

    Added product serves

--- apm/src/app/app.module.ts
+++ apm/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,8 @@ import { StarComponent } from './shared/star.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
